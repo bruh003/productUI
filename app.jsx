0029f8a48d10bb586237dd45e6e4d2d5bc5bfb23
bruh003 <81505248+bruh003@users.noreply.github.com>
2025-02-24 16:54:45 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ProductsUI from './Components/ProductsUI';
+
+const App = () => {
+  return (
+    <div>
+      <ProductsUI/>
+      
+    </div>
+  )
+}
+
+export default App
